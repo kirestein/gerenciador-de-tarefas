fe06657a1cb07824594963a9efeda1792c223a15
kirestein <erik.proenca2011@gmail.com>
2021-09-18 15:16:16 -0300

    Criando a diretiva tarefa-concluida

--- src/app/tarefas/shared/shared.module.ts
+++ src/app/tarefas/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TarefaConcluidaDirective } from './tarefa-concluida.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TarefaConcluidaDirective
+  ],
   imports: [
     CommonModule
   ]
