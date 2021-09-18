bb8b9173ab5730f7d256d27c92c927238e4c5112
kirestein <erik.proenca2011@gmail.com>
2021-09-18 14:51:29 -0300

    implementando o método alterarStatus

--- src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
+++ src/app/tarefas/listar-tarefa/listar-tarefa.component.ts
@@ -28,4 +28,10 @@ export class ListarTarefaComponent implements OnInit {
     }
   }
 
+  alterarStatus(tarefa: Tarefa): void {
+    if(confirm('Deseja alterar o status da tarefa ' + tarefa.nome))
+    this.tarefaService.alterarStatus(tarefa.id);
+    this.tarefas = this.tarefaService.listarTodos();
+  }
+
 }
